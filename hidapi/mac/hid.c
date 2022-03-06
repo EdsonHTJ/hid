2114c1b9c851203dfaa900cda72032adbb46628d
EdsonHTJ <edsonsobralense@gmail.com>
2022-03-06 17:23:11 -0300

    reverting define to if

--- hidapi/mac/hid.c
+++ hidapi/mac/hid.c
@@ -192,7 +192,7 @@ static struct hid_api_version api_version = {
 
 static	IOHIDManagerRef hid_mgr = 0x0;
 static	int is_macos_10_10_or_greater = 0;
-mach_port_t io_port;
+mach_port_t *io_port;
 
 
 #if 0
@@ -347,16 +347,11 @@ int HID_API_EXPORT hid_init(void)
 	if (!hid_mgr) {
 		is_macos_10_10_or_greater = (NSAppKitVersionNumber >= 1343); /* NSAppKitVersionNumber10_10 */
 		short isMacos12Available = 0;
-		short isAvailable = 0;
 		if (__builtin_available(macOS 12, *)) {
-			isAvailable = 1;
+			io_port = (mach_port_t *) &kIOMainPortDefault;
+		}else {
+			io_port = (mach_port_t *) &kIOMasterPortDefault;
 		}
-
-		#if isAvailable 
-			#define IO_PORT kIOMainPortDefault
-		#else 
-			#define IO_PORT kIOMasterPortDefault
-		#endif
 
 		return init_hid_manager();
 	}
@@ -806,12 +801,12 @@ static io_registry_entry_t hid_open_service_registry_from_path(const char *path)
 		char *endptr;
 		uint64_t entry_id = strtoull(path + 10, &endptr, 10);
 		if (*endptr == '\0') {
-			return IOServiceGetMatchingService(IO_PORT, IORegistryEntryIDMatching(entry_id));
+			return IOServiceGetMatchingService(*io_port, IORegistryEntryIDMatching(entry_id));
 		}
 	}
 	else {
 		/* Fallback to older format of the path */
-		return IORegistryEntryFromPath(IO_PORT, path);
+		return IORegistryEntryFromPath(*io_port, path);
 	}
 
 	return MACH_PORT_NULL;
